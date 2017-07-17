7baaf918459dade0d2ada2e2d3dd9f58b81f5aae
c-bata <contact@c-bata.link>
2017-07-18 05:19:13 +0900

    Add FilterHasPrefix and FilterContains

---
+++ prompt/filter.go
@@ -0,0 +1,68 @@
+package prompt
+
+import "strings"
+
+func FilterHasPrefix(completions []string, sub string, ignoreCase bool) []string {
+	if sub == "" {
+		return completions
+	}
+	if ignoreCase {
+		sub = strings.ToUpper(sub)
+	}
+
+	ret := make([]string, 0, len(completions))
+	for i := range completions {
+		n := completions[i]
+		if ignoreCase {
+			n = strings.ToUpper(n)
+		}
+		if strings.HasPrefix(n, sub) {
+			ret = append(ret, completions[i])
+		}
+	}
+	return ret
+}
+
+func FilterHasSuffix(completions []string, sub string, ignoreCase bool) []string {
+	if sub == "" {
+		return completions
+	}
+	if ignoreCase {
+		sub = strings.ToUpper(sub)
+	}
+
+	ret := make([]string, 0, len(completions))
+	for i := range completions {
+		n := completions[i]
+		if ignoreCase {
+			n = strings.ToUpper(n)
+		}
+		if strings.HasSuffix(n, sub) {
+			ret = append(ret, completions[i])
+		}
+	}
+	return ret
+}
+
+func FilterContains(completions []string, sub string, ignoreCase bool) []string {
+	if sub == "" {
+		return completions
+	}
+	if ignoreCase {
+		sub = strings.ToUpper(sub)
+	}
+
+	ret := make([]string, 0, len(completions))
+	for i := range completions {
+		n := completions[i]
+		if ignoreCase {
+			n = strings.ToUpper(n)
+		}
+		if strings.Contains(n, sub) {
+			ret = append(ret, n)
+		}
+	}
+	return ret
+}
+
+
