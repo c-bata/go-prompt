ba233229562fbeebe1328788a6245cda9124af27
c-bata <contact@c-bata.link>
2017-08-22 18:22:55 +0900

    Update example

--- _example/input/main.go
+++ _example/input/main.go
@@ -6,10 +6,23 @@ import (
 	"github.com/c-bata/go-prompt"
 )
 
-func main() {
-	fruits := []string{"Apple", "Banana", "Bilberry", "Coconuts"}
+func completer(in prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "users", Description: "Store the username and age"},
+		{Text: "articles", Description: "Store the article text posted by user"},
+		{Text: "comments", Description: "Store the text commented to articles"},
+		{Text: "groups", Description: "Combine users with specific rules"},
+	}
+	return prompt.FilterHasPrefix(s, in.GetWordBeforeCursor(), true)
+}
 
-	fmt.Println("What fruits do you like?")
-	f := prompt.Choose("> ", fruits)
-	fmt.Println("You like " + f)
+func main() {
+	in := prompt.Input(">>> ", completer,
+		prompt.OptionTitle("sql-prompt"),
+		prompt.OptionHistory([]string{"SELECT * FROM users;"}),
+		prompt.OptionPrefixTextColor(prompt.Yellow),
+		prompt.OptionPreviewSuggestionTextColor(prompt.Blue),
+		prompt.OptionSelectedSuggestionBGColor(prompt.LightGray),
+		prompt.OptionSuggestionBGColor(prompt.DarkGray))
+	fmt.Println("Your input: " + in)
 }
