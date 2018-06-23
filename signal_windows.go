d7dd5f9548c450091987c95b3df426a9accc41b0
c-bata <contact@c-bata.link>
2018-06-24 04:39:19 +0900

    Use context.Context and Cancel function

--- signal_windows.go
+++ signal_windows.go
@@ -3,13 +3,14 @@
 package prompt
 
 import (
+	"context"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
-func (p *Prompt) handleSignals(exitCh chan int, winSizeCh chan *WinSize, stop chan struct{}) {
+func (p *Prompt) handleSignals(tx context.Context, cancel context.CancelFunc, winSizeCh chan *WinSize) {
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(
 		sigCh,
@@ -20,23 +21,18 @@ func (p *Prompt) handleSignals(exitCh chan int, winSizeCh chan *WinSize, stop ch
 
 	for {
 		select {
-		case <-stop:
-			log.Println("[INFO] stop handleSignals")
+		case <-ctx.Done():
 			return
 		case s := <-sigCh:
 			switch s {
-
 			case syscall.SIGINT: // kill -SIGINT XXXX or Ctrl+c
-				log.Println("[SIGNAL] Catch SIGINT")
-				exitCh <- 0
+				cancel()
 
 			case syscall.SIGTERM: // kill -SIGTERM XXXX
 				log.Println("[SIGNAL] Catch SIGTERM")
-				exitCh <- 1
 
 			case syscall.SIGQUIT: // kill -SIGQUIT XXXX
-				log.Println("[SIGNAL] Catch SIGQUIT")
-				exitCh <- 0
+				cancel()
 			}
 		}
 	}
